b6bca76d6ac404571f36bd88a6af16711bf79496
Dmitry Toshchev <dterron@yandex.ru>
2018-05-19 23:30:21 +0300

    initial commit

---
+++ app/js/common.js
@@ -0,0 +1,31 @@
+$(function() {
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 0) {
+			$('#nav').css('background-color', '#138fc2');
+		} else {
+			$('#nav').css('background-color', '');
+		}
+	});
+
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+		nav:false,
+		autoplay:true,
+		dots:false,
+		navText : ['next ',' prev'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+	});
+	
+});
+
